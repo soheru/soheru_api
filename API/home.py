bc57605785a1d7b1eb7334cf3d5d5dd58d094bfc
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 09:00:16 -0700

    Update home.py

--- API/home.py
+++ API/home.py
@@ -1,6 +1,10 @@
 import requests, random
-from API import app 
+from API import app, db
 from flask import redirect, jsonify
+from datetime import datetime
+from API.routes import generate_short_id
+from API.models import ShortUrls
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 
 @app.route('/')
@@ -11,4 +15,11 @@ def mainpage():
 def alphacoders(query):
     x = requests.get(f"https://wall.alphacoders.com/api2.0/get.php?auth=6950f559377140a4e1594c564cdca6a3&method=search&term={query}", headers=headers).json().get('wallpapers')
     y = random.choice(x)
-    return jsonify({'url_image':y.get('url_image'), 'thumb_url':y.get('url_thumb')})
+    
+    short_id = generate_short_id(8)
+    thumb = ShortUrls(original_url=y.get('url_thumb'), short_id=short_id, created_at=datetime.now())
+    urlimage = ShortUrls(original_url=y.get('url_image'), short_id=short_id, created_at=datetime.now())
+    db.session.add(thumb)
+    db.session.add(urlimage)
+    db.session.commit()
+    return jsonify({'url_image':urlimage, 'thumb_url':thumb})
