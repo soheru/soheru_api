119b31336b5579a848e3f8ab14a018645b7eac0d
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 05:19:56 -0700

    Update images.py

--- images.py
+++ images.py
@@ -2,15 +2,16 @@ import os
 from bing_image_downloader import downloader
 
 def image_bing(query, images:int):
+    
     downloader.download(
         query,
         limit=images,
-        output_dir="static/store",
+        output_dir="./static/store",
         adult_filter_off=False,
         force_replace=False,
         timeout=180,
     )
     kes = []
-    for x in os.listdir('static/store/'+query):
-        kes.append(f'http://api.soheru.in/images/static/store/{query}/{x}')
+    for x in os.listdir('./static/store/'+query):
+        kes.append(f'api.soheru.in/images/static/store/{query}/{x}')
     return kes
