a53e486a8dcd3f08594b6ea16004714237025d8f
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 09:30:57 -0700

    Update routes.py

--- API/routes.py
+++ API/routes.py
@@ -11,6 +11,30 @@ def generate_short_id(num_of_chars: int):
     return ''.join(choice(string.ascii_letters+string.digits) for _ in range(num_of_chars))
 
 
+@app.route('/api/short', methods=['POST'])
+def api_short():
+    if request.method == 'POST':
+        url = request.form['url']
+        short_id = request.form['custom_id']
+        
+        if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
+            return 'Please enter different custom id!'    
+        
+        if not url:
+            return 'The URL is required!'
+        
+        if not short_id:
+            short_id = generate_short_id(8)
+        
+        new_link = ShortUrls(
+            original_url=url, short_id=short_id, created_at=datetime.now())
+        db.session.add(new_link)
+        db.session.commit()
+        short_url = request.host_url + "short/" + short_id
+        return short_url    
+            
+            
+
 @app.route('/short', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
