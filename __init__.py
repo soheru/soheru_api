70464c5ae6173e29f553e1534895e9cda993b76d
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 03:46:58 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,36 +9,36 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0)
 
 @app.route('/')
 def mainpage():
-    return {'success':True, 'status':'working'}
+    return jsonify({'success':True, 'status':'working'})
 
 @app.route('/alpha/<query>')
 def alphacoders(query):
     x = requests.get(f"https://wall.alphacoders.com/api2.0/get.php?auth=6950f559377140a4e1594c564cdca6a3&method=search&term={query}", headers=headers).json().get('wallpapers')
     y = random.choice(x)
-    return {'url_image':y.get('url_image'), 'thumb_url':y.get('url_thumb')}
+    return jsonify({'url_image':y.get('url_image'), 'thumb_url':y.get('url_thumb')})
 
 @app.route('/tmdb/v1/<query>')
 def tmdb_v1(query):
     x = get_shows(query)[0]
-    return get_beauitfy_details(x)
+    return jsonify(get_beauitfy_details(x))
 
 @app.route('/tmdb/<query>')
 def tmdb_whole_raw(query):
     x = get_shows(query)
-    return x[1]
+    return jsonify(x[1])
 
 @app.route('/tmdb/raw/<query>')
 def tmdb_raw(query):
     x = get_shows(query)
-    return get_raw_tmdb(x[0])    
+    return jsonify(get_raw_tmdb(x[0]))    
 
 @app.route('/anime/<query>')
 def anime_api(query):
-    return anime_info(query)
+    return jsonify(anime_info(query))
 
 @app.route('/manga/<query>')
 def manga_api(query):
-    return manga_info(query)
+    return jsonify(manga_info(query))
 
 if __name__ == '__main__':
     app.run()
