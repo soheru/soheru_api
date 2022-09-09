7f629b2d4804b197407bcb37b5a2d3086bb059ee
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 10:05:41 -0700

    Update routes.py

--- API/routes.py
+++ API/routes.py
@@ -9,7 +9,31 @@ from flask import render_template, request, flash, redirect, url_for
 def generate_short_id(num_of_chars: int):
     """Function to generate short_id of specified number of characters"""
     return ''.join(choice(string.ascii_letters+string.digits) for _ in range(num_of_chars))
-     
+
+
+@app.route('/apishort', methods=['POST'])
+def api_short():
+    if request.method == 'POST':
+        url = request.args.get('url')
+        short_id = request.args.get('custom_id')
+        
+        if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
+            return 'Please enter different custom id!'    
+        
+        if not url:
+            return 'The URL is required!'
+        
+        if not short_id:
+            short_id = generate_short_id(8)
+        
+        new_link = ShortUrls(
+            original_url=url, short_id=short_id, created_at=datetime.now())
+        db.session.add(new_link)
+        db.session.commit()
+        short_url = request.host_url + "short/" + short_id
+        return {'url':short_url}    
+            
+            
 
 @app.route('/short', methods=['GET', 'POST'])
 def index():
