54a2ac901037112333f79ebc8d00b9417746e19b
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 06:49:12 -0700

    dxd

--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0)
 def devian(query):
     x = requests.get(f"https://www.deviantart.com/search?q={query}", headers=headers).content
     y = bs(x, "html.parser")
-    final = y.find_all('img', src=re.compile('https://images-wixmp'))
-    return final
-
+    ks = []
+    for item in y.find_all('img', src=re.compile('https://images-wixmp')):
+        ks.append(item['src'])  
+    return ks    
+          
