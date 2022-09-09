8b28d4ad45e95c961a26b601cbcba82744be595c
Soheru San <contact.sohail.me@gmail.com>
2022-09-09 08:38:21 -0700

    done

--- API/__init__.py
+++ API/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['FLASK_APP']='API'
+app.config['DATABASE_URL'] = 'postgres://xmfzqksv:DwEFghfDu6PSqI0umNTQImL_PcIsNsdv@jelani.db.elephantsql.com/xmfzqksv'
+app.config['SECRET_KEY']='verysecretkey'
+db = SQLAlchemy(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
+migrate = Migrate(app, db)
 
 from API import home
 from API import anime 
 from API import devian
 from API import tmdb 
+from API import routes
+from API import models
